63c97a8c373190f4a45b26c85a28864e2658854b
MirandaMG20 <guadamiran@gmail.com>
2023-08-07 11:23:56 -0400

    api test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
-function App() {
+function Books() {
+
+  const [book, setBook] = useState(null);
+
+  const url = 'https://book-finder1.p.rapidapi.com/api/search?title=the%20notebook&author=Nicholas%20Sparks&book_type=Fiction&results_per_page=10&page=1';
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
+      'X-RapidAPI-Host': 'book-finder1.p.rapidapi.com'
+    }
+  };
+
+  // Function to getBooks
+  const getBook = async (searchTerm) => {
+
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+
+      console.log(data)
+
+      // setBook(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getBook("The Notebook");
+    console.log('useEffect is called')
+  }, []);
+
 
   return (
     <>
-        <h1>Books</h1>
+      <h1>Books</h1>
     </>
   )
 }
 
-export default App
+export default Books
